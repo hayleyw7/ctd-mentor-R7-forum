7b8297854edaa1d5c0e002c1af0356d6987fa1dd
Hayley Witherell <hayleywitherell@gmail.com>
2024-07-15 18:38:22 -0600

    Complete step 2a

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_16_003429) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_16_003553) do
   create_table "forums", force: :cascade do |t|
     t.string "forum_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
   end
 
 end
