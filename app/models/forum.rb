5b75e95ea29c049d5eb45ae460ad6893313d96b9
Hayley Witherell <hayleywitherell@gmail.com>
2024-08-02 17:34:01 -0600

    Fix nav around forum

--- app/models/forum.rb
+++ app/models/forum.rb
@@ -2,4 +2,7 @@ class Forum < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :subscriptions
   has_many :users, through: :subscriptions
+
+  validates :forum_name, presence: true
+  validates :description, presence: true
 end
